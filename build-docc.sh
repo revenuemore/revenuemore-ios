8f15016c924aed9e5dbaa533fdc0295c19bafae7
Bilal Durnagol <bilaldurnagol@gmail.com>
2025-01-20 16:39:44 +0300

    script was included

---
+++ build-docc.sh
@@ -0,0 +1,13 @@
+##!/bin/sh
+
+xcrun xcodebuild docbuild \
+	-scheme RevenueMore \
+	-destination 'generic/platform=iOS Simulator' \
+	-derivedDataPath "$PWD/.derivedData"
+	
+xcrun docc process-archive transform-for-static-hosting \
+	"$PWD/.derivedData/Build/Products/Debug-iphonesimulator/RevenueMore.doccarchive" \
+	--output-path ".docs" \
+	--hosting-base-path "" # add your repo name later
+	
+echo '<script>window.location.href += "/documentation/revenuemore"</script>' > .docs/index.html
