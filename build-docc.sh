0342326c7c73fa12ecc65dca28214a46b0303728
Bilal Durnagol <bilaldurnagol@gmail.com>
2025-01-21 18:25:35 +0300

    script was fixed

--- build-docc.sh
+++ build-docc.sh
@@ -1,13 +1,14 @@
 ##!/bin/sh
 
 xcrun xcodebuild docbuild \
-	-scheme RevenueMore \
-	-destination 'generic/platform=iOS Simulator' \
-	-derivedDataPath "$PWD/.derivedData"
-	
+    -scheme RevenueMore \
+    -destination 'generic/platform=iOS Simulator' \
+    -derivedDataPath "$PWD/.derivedData"
+
+
 xcrun docc process-archive transform-for-static-hosting \
-	"$PWD/.derivedData/Build/Products/Debug-iphonesimulator/RevenueMore.doccarchive" \
-	--output-path ".docs" \
-	--hosting-base-path "https://revenuemore.github.io" 
-	
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/RevenueMore.doccarchive" \
+    --output-path ".docs" \
+    --hosting-base-path "https://<revenuemore.github.io/revenuemore-ios"
+
 echo '<script>window.location.href += "/documentation/revenuemore"</script>' > .docs/index.html
